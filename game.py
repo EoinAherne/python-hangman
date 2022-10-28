f94ec9aebb72046c88e4da2be9079c167990d023
EoinAherne <aherneeoin88@gmail.com>
2022-10-28 14:08:35 +0000

    add for loop to iterate through guess values and return word location

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import random
 print('Welcome to the game. Lets begin!')
 print('--------------------------------')
 
-words = [start, hangman, college, game, computer, application, hardware, portfolio, course]
+words = ['start', 'hangman', 'college', 'game', 'computer', 'application', 'hardware', 'portfolio', 'course']
 
 class game:
     def __init__(self):
@@ -14,3 +14,11 @@ class game:
     def show(self):
         display = ' '.join(self.display)
         print(f'The answer is: {display}')
+
+    def get_word(self, guess):
+        word_location = []
+
+        for index, char in enumerate(list(self.word)):
+            if char == guess:
+                word_location.append(index)
+        return word_location
