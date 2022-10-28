a4d20106e9ae52d94eff18f45ce2e865d2e3f8d9
EoinAherne <aherneeoin88@gmail.com>
2022-10-28 14:17:39 +0000

    Add check guess function to game

--- game.py
+++ game.py
@@ -11,18 +11,26 @@ class game:
         self.display = ['_' for letter in self.word]
         self.guesses = 0
 
+
     def show(self):
         display = ' '.join(self.display)
         print(f'The answer is: {display}')
 
+
     def get_word(self, guess):
         word_location = []
-
         for index, char in enumerate(list(self.word)):
             if char == guess:
                 word_location.append(index)
         return word_location
 
+
     def update(self, idx, letter):
         for number in idx:
             self.display[idx] = letter
+
+
+    def check_guess(self, guess):
+        if guess in self.word:
+            if guess == self.get_word(guess)
+            self.update(idx, guess)
