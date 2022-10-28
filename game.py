2dad9ff6344fbebf028a1442b6907c472fc987f1
EoinAherne <aherneeoin88@gmail.com>
2022-10-28 13:59:26 +0000

    Add show function

--- game.py
+++ game.py
@@ -3,10 +3,14 @@ import random
 print('Welcome to the game. Lets begin!')
 print('--------------------------------')
 
-words = [project, start, hangman, college, game, computer, application, hardware, portfolio, course]
+words = [start, hangman, college, game, computer, application, hardware, portfolio, course]
 
 class game:
     def __init__(self):
         self.word = random.choice(words)
         self.display = ['_' for letter in self.word]
         self.guesses = 0
+
+    def show(self):
+        display = ' '.join(self.display)
+        print(f'The answer is: {display}')
